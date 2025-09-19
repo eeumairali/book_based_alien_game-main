0faea8f8c766f533c447be6c7dc074358e9afeb3
umair <eeumairali@gmail.com>
2025-09-19 17:51:43 +0500

    book based rocket alient game in python pygame

---
+++ game_function.py
@@ -0,0 +1,43 @@
+import sys
+import pygame
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+
+
+def check_event(ai_settings,screen,ship,bullets):
+    """this function will check events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
+
+        
+
+def update_screen(ai_setting,screen, ship, bullets):
+    screen.fill(ai_setting.bg_color)
+    ship.blitme()
+
+    for bullet in bullets.sprites():
+        print("firing")
+        bullet.draw_bullet()
+    
+    pygame.display.flip()
